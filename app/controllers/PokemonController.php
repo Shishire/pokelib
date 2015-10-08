21becf820c2bd82f01e8b7f985b0c0a3cd1716b5
Wesley Hirsch <emperorshishire@gmail.com>
2015-10-08 02:59:31 +0000

    Added Download functionality

--- app/controllers/PokemonController.php
+++ app/controllers/PokemonController.php
@@ -11,4 +11,14 @@ class PokemonController extends \lithium\action\Controller
 		$pokemon = PokemonFiles::find($this->request->id);
 		return compact('pokemon');
 	}
+	
+	public function download()
+	{
+		$pokemon = PokemonFiles::find($this->request->id);
+		header('Content-Type: application/octet-stream');
+		header('Content-Length: '.strlen($pokemon->file->bin));
+		header('Content-Disposition: attachment; filename="'.$pokemon->getNickname().' - '.strtoupper(dechex($pokemon->getPersonality())).'.pk6"');
+		echo $pokemon->file->bin;
+		exit();
+	}
 }
--- app/views/pokemon/view.html.php
+++ app/views/pokemon/view.html.php
@@ -1,5 +1,5 @@
-<p>File Size: <?=$pokemon->fileSize?></p>
 <div style="word-wrap: break-word">
+    <?=$this->html->link('Download', array('controller' => 'pokemon', 'action' => 'download', 'id' => $pokemon->_id));?><br />
     Nickname: <?=$pokemon->nickname?><br />
     Dex Id: <?=$pokemon->dexId?>
 </div>
